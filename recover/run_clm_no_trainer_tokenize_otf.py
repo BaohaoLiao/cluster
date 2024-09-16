08b7aa7b9036bc697893e8982c1798a2bfd5af2a
baohaoliao <liaobaohao@gmail.com>
2024-09-16 12:24:51 +0200

    debug

--- recover/run_clm_no_trainer_tokenize_otf.py
+++ recover/run_clm_no_trainer_tokenize_otf.py
@@ -679,7 +679,8 @@ def main():
     
 
     logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {len(train_dataset)}")
+    if not args.streaming:
+        logger.info(f"  Num examples = {len(train_dataset)}")
     logger.info(f"  Num Epochs = {args.num_train_epochs}")
     logger.info(f"  Instantaneous batch size per device = {args.per_device_train_batch_size}")
     logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
